c3641c1df9e6765f3d1f82091d5a856f0988ddd5
Carlos Philippe <carl2678@yahoo.fr>
2022-11-21 11:24:13 -0500

    Add script js for menu mobile

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+const hamburger = document.querySelector('.hamburger');
+hamburger.addEventListener('click', () => {
+    const div = document.createElement('div');
+    const img = document.createElement('img');
+    const links = document.querySelector('.internal-link');
+    const hero = document.querySelector('.hero');
+    const divWrap = document.createElement('div');
+    const linksChilds = Array.from(links.children);
+    body.style.overflow = 'hidden';
+}
