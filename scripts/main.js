9496b3209e929ada1a13f3efe21a9ba0c067a896
José Fernández-Caminero <fdezcaminero@gmail.com>
2022-11-23 13:58:50 -0400

    Create JS functionality

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+import {
+  menuIconMobile, menuLinks, openMenu, closeMenu,
+} from './menu.js';
+
+menuIconMobile.addEventListener('click', openMenu);
+menuLinks.forEach((menuLink) => {
+menuLink.addEventListener('click', closeMenu);
+});
